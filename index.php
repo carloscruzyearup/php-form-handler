b42b4d1e492ba8ffc7b8414f4ca7b8838c376fac
carloscruzyearup <carlos.cruz3@seattlecolleges.edu>
2022-05-31 11:26:53 -0700

    Remove unnecessary input and check box

--- index.php
+++ index.php
@@ -75,15 +75,6 @@ if (isset($_POST["FirstName"])) {
 								<label for="firstname">First Name:</label>
 								<input type="text" class="form-control" id="firstname" placeholder="Enter first name" name="FirstName">
 							</div>
-							<div class="mb-3">
-								<label for="lastname">Last Name:</label>
-								<input type="text" class="form-control" id="lastname" placeholder="Enter last name" name="LastName">
-							</div>
-							<div class="form-check mb-3">
-								<label class="form-check-label">
-									<input class="form-check-input" type="checkbox" name="remember"> Remember me
-								</label>
-							</div>
 							<button type="submit" class="btn btn-primary">Submit</button>
 						</form>
 					';
